afd4df4488fda233bc8e555cfba36049a8832e6e
SaulMendoza212 <quitosaul2006@gmail.com>
2024-12-05 15:26:55 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Cambia el color de fondo al hacer clic en el botón
+document.getElementById("changeColorButton").onclick = function() {
+    document.body.style.backgroundColor = "#f39c12";  // Color de fondo
+};
