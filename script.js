26426e3d2093e3854a8a40392696ee5b01874f1e
SaulMendoza212 <quitosaul2006@gmail.com>
2024-12-08 21:21:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 document.getElementById("changeColorButton").onclick = function() {
     document.body.style.backgroundColor = "#f39c12";  // Color de fondo
 };
+
+// Mostrar el botón de scroll al principio cuando se desplaza hacia abajo
+window.onscroll = function() {
+    var button = document.querySelector('.scroll-to-top');
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        button.style.display = "block";  // Mostrar el botón
+    } else {
+        button.style.display = "none";  // Ocultar el botón
+    }
+};
